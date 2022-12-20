7c3cc6cf3181f935caf7ad9050598ff1cca6a1d3
o4kas12 <ochkasdima92@yandex.ru>
2022-12-20 14:46:15 +0300

    add script to check differece for 2 different bases

--- checkDifference2Bases.py
+++ checkDifference2Bases.py
@@ -4,13 +4,15 @@ missed_codes = []
 conn2 = psycopg2.connect(dbname='molzavod', user='postgres', password='111111', host='10.10.3.109')
 cursor2 = conn2.cursor()
 
-cursor2.execute("select gtin, tail from marking.line where batch_date = '2022-12-02' and line = 'A1-13' and cod_gp = '31220' and verified_status='verified'")
+cursor2.execute("select gtin, tail from marking.line where batch_date = '2022-12-02' "
+                "and line = 'A1-13' and cod_gp = '31220' and verified_status='verified'")
 data2 = cursor2.fetchall()
 
 conn = psycopg2.connect(dbname='molzavod', user='postgres', password='111111', host='192.168.240.5')
 cursor = conn.cursor()
 
-cursor.execute("select gtin, tail from marking.line where batch_date = '2022-12-02' and line = 'A1-13' and cod_gp = '31220' and verified_status='verified'")
+cursor.execute("select gtin, tail from marking.line where batch_date = '2022-12-02' "
+               "and line = 'A1-13' and cod_gp = '31220' and verified_status='verified'")
 data = cursor.fetchall()
 
 for i in range(len(data)):
